79be91210ea9aeafb57d4982c41f3033f6616741
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2024-10-14 16:11:21 +0800

    edited tetris.py, board.py, main.py

--- board.py
+++ board.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pygame
+import shape 
 
 class Board:
     
@@ -9,3 +11,14 @@ class Board:
         self._matrix = np.zeros([width, height], dtype=int)
         self._current_tile = None
         self._score = 0
+        self._colors = shape.generate_colours()
+        self._shapes = shape.generate_shapes()
+
+    def draw(self):
+        blockSize = 35 
+        x_offset = 100
+        y_offset = 50
+        for x in range(0, self._width):
+            for y in range(0, self._height):
+                rect = pygame.Rect(x_offset + x * blockSize, y_offset + y * blockSize, blockSize, blockSize)
+                pygame.draw.rect(self._screen, self._colors[self._matrix[x, y]], rect)
---
+++ main.py
@@ -0,0 +1,6 @@
+import pygame
+import shape
+
+from tetris import Tetris
+
+Tetris().run()
